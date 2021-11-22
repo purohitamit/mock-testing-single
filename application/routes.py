dd8e24ea41624b85581dc235d00a0672ec2e2701
Ubuntu <azureuser@myVM.5qh0wf5nsbruhlksmk54necr3c.zx.internal.cloudapp.net>
2021-11-22 18:35:03 +0000

    pushing to github

---
+++ application/routes.py
@@ -0,0 +1,14 @@
+from application import app
+import requests
+
+@app.route('/get/sport', methods=['GET'])
+def sport():
+    response = requests.get('http://api:5000/get/number')
+    if response.text == "1":
+        return "Football"
+    elif response.text == "2":
+        return "Badminton"
+    elif response.text == "3":
+        return "Hockey"
+    else:
+        return "Boxing"
---
+++ testing/test_mock.py
@@ -0,0 +1,41 @@
+from unittest.mock import patch
+from flask import url_for
+from flask_testing import TestCase
+
+from application import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_football(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with patch('requests.get') as g:
+            g.return_value.text = "1"
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Football', response.data)
+    def test_badminton(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with patch('requests.get') as g:
+            g.return_value.text = "2"
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Badminton', response.data)
+    def test_hockey(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with patch('requests.get') as g:
+            g.return_value.text = "3"
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Hockey', response.data)
+    def test_boxing(self):
+    # We will mock a response of 1 and test that we get football returned.
+        with patch('requests.get') as g:
+            g.return_value.text = "4"
+
+            response = self.client.get(url_for('sport'))
+            self.assertIn(b'Boxing', response.data)
+    
